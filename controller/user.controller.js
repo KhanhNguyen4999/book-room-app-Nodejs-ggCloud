d3177803d7212e74d7db685f4a0845d38966db6f
khanhnguyen <nguyenngocminhkhanh4999@gmail.com>
2021-06-28 12:14:13 +0700

    update function getListFavourite

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -14,7 +14,7 @@ module.exports.registerUser = (req, res) => {
         console.log(req.files.length)
         try {
             let data;
-            if (req.files.length!=0){
+            if (req.files.length != 0) {
                 const imageUrl = await helpers.uploadImage(req.files[0])
                 console.log({
                     message: "Upload was successful",
@@ -26,7 +26,7 @@ module.exports.registerUser = (req, res) => {
                     avatar: imageUrl
                 }
             }
-            else{
+            else {
                 console.log("gdf")
                 data = {
                     username: req.body.username,
@@ -143,8 +143,8 @@ module.exports.addFavourite = (req, res) => {
 };
 
 module.exports.removeProductInListFavourite = (req, res) => {
-    (async() =>{
-        try{
+    (async () => {
+        try {
             let user = db.collection('user').doc(req.body.idUser);
             let data = await user.get();
             data = data.data();
@@ -154,8 +154,8 @@ module.exports.removeProductInListFavourite = (req, res) => {
             console.log(data);
             let ls_favourite = data['favouriteList']
             let index = ls_favourite.indexOf(req.body.idProduct)
-           
-            if(index>-1){
+
+            if (index > -1) {
                 ls_favourite.splice(index, 1)
                 const res = await user.update({
                     favouriteList: ls_favourite
@@ -164,7 +164,7 @@ module.exports.removeProductInListFavourite = (req, res) => {
             }
             return res.status(200).send("Product have not in list")
         }
-        catch(error){
+        catch (error) {
             return res.status(500).send(error);
         }
     })()
@@ -176,19 +176,23 @@ module.exports.getListFavourite = (req, res) => {
             let user = db.collection('user').doc(req.params.id);
             user = await user.get();
             user = user.data();
-            let favouriteList = user['favouriteList'];
-
-            let product = db.collection('product')
-            console.log(favouriteList)
-            let response = []
-            for(let p of favouriteList){
-                let rel = product.doc(p);
-                rel = await rel.get();
-                rel = rel.data();
-                rel['id'] = p;
-                response.push(rel);
+            let response = [];
+            console.log(user)
+            if ('favouriteList' in user) {
+                console.log("inside")
+                let favouriteList = user['favouriteList'];
+                console.log(favouriteList)
+                let product = db.collection('product')
+                console.log(product)
+                console.log(favouriteList)
+                for (let p of favouriteList) {
+                    let rel = product.doc(p);
+                    rel = await rel.get();
+                    rel = rel.data();
+                    rel['id'] = p;
+                    response.push(rel);
+                }
             }
-            
             return res.status(200).send(response)
         }
         catch (err) {
@@ -198,21 +202,21 @@ module.exports.getListFavourite = (req, res) => {
     })()
 }
 
-module.exports.validateLogin = (req, res) =>{
+module.exports.validateLogin = (req, res) => {
     let error = []
 
-    if(!req.body.username)
+    if (!req.body.username)
         error.push("username is required")
-    
-    if(!req.body.password)
-        error.push("password is required")
-    
-    if(error.length)
-        return res.status(200).send(error);
-    
 
-    (async() =>{
-        try{
+    if (!req.body.password)
+        error.push("password is required")
+
+    if (error.length)
+        return res.status(200).send(error);
+
+
+    (async () => {
+        try {
             let users = db.collection('user');
             let response = []
             await users.get().then(querySnapshot => {
@@ -220,75 +224,75 @@ module.exports.validateLogin = (req, res) =>{
                 for (let doc of docs) {
                     let id = doc.id;
                     selectedUser = doc.data();
-                    if (selectedUser['username'] == req.body.username && selectedUser['password'] == req.body.password){
+                    if (selectedUser['username'] == req.body.username && selectedUser['password'] == req.body.password) {
                         selectedUser['id'] = id;
                         return res.status(200).send(selectedUser);
                     }
-                    
+
                 }
                 return res.status(500).send('Invalid username or password');
             })
 
         }
-        catch(error){
+        catch (error) {
             return res.status(500).send("Error")
         }
     })()
 }
 
-module.exports.makeAnAppointment = (req, res) =>{
-    (async()=>{
-        try{
-        // Handle host
-        let host = db.collection('user').doc(req.body.hostId)
-        let booking ={
-            'fullname': req.body.fullname,
-            'email': req.body.email,
-            'phone': req.body.phone,
-            'date': req.body.date
-        }
+module.exports.makeAnAppointment = (req, res) => {
+    (async () => {
+        try {
+            // Handle host
+            let host = db.collection('user').doc(req.body.hostId)
+            let booking = {
+                'fullname': req.body.fullname,
+                'email': req.body.email,
+                'phone': req.body.phone,
+                'date': req.body.date
+            }
 
-        let hostData = await host.get();
-        hostData = hostData.data();
-        if('booking' in hostData){
-            hostData['booking'].push(booking)
-            const res = await host.update({
-                booking: hostData['booking']
-            });
-    
-        }
-        else{
-            const t = await host.set({
-                booking: [booking]
-            }, { merge: true });    
-        }
+            let hostData = await host.get();
+            hostData = hostData.data();
+            if ('booking' in hostData) {
+                hostData['booking'].push(booking)
+                const res = await host.update({
+                    booking: hostData['booking']
+                });
 
-        // Handle user
-        let user = db.collection('user').doc(req.body.userId)
-        let room_contacted = {
-            'productId': req.body.productId,
-            'date': req.body.date 
+            }
+            else {
+                const t = await host.set({
+                    booking: [booking]
+                }, { merge: true });
+            }
+
+            // Handle user
+            let user = db.collection('user').doc(req.body.userId)
+            let room_contacted = {
+                'productId': req.body.productId,
+                'date': req.body.date
+            }
+            let userData = await user.get();
+            userData = userData.data();
+            if ('room_contacted' in userData) {
+                userData['room_contacted'].push(room_contacted)
+                const res = await user.update({
+                    room_contacted: userData['room_contacted']
+                });
+
+            }
+            else {
+                const t = await user.set({
+                    room_contacted: [room_contacted]
+                }, { merge: true });
+            }
+
+            return res.status(200).send("Make an appointment successfully")
         }
-        let userData = await user.get();
-        userData = userData.data();
-        if('room_contacted' in userData){
-            userData['room_contacted'].push(room_contacted)
-            const res = await user.update({
-                room_contacted: userData['room_contacted']
-            });
-    
+        catch (error) {
+            return res.status(500).send("Error")
         }
-        else{
-            const t = await user.set({
-                room_contacted: [room_contacted]
-            }, { merge: true });    
-        }
-        
-        return res.status(200).send("Make an appointment successfully")
-    }
-    catch(error){
-        return res.status(500).send("Error")
-    }
 
     })()
 }
